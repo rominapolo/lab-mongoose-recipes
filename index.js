715c74128573f8d8ad35f64c8f2d6d541d38bbc0
Romina Polo <rominalpolo@gmail.com>
2022-09-13 13:17:20 -0400

    lab almost done

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -16,8 +19,41 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    
+    const myNewRecipe = {
+      title: "Pizza",
+      level: "Easy Peasy",
+      ingredients: ["cheese", "pep", "onions", "sausage"],
+      cuisine: "Italian"
+    }
+    Recipe.create(myNewRecipe).then( () => {
+      console.log({title: "pizza"});
+      mongoose.disconnect();
+    }).catch(err => {
+      mongoose.disconnect();
+      throw err;
+      })
+    
+    Recipe.create(data).then(myNewRecipe => {
+      console.log('new recipes', myNewRecipe );
+    }).then(() => {
+     mongoose.disconnect();
+     }).catch(err => {
+      mongoose.disconnect();
+      throw err;
+    })
+
+  //   Recipe.findByIdAndUpdate("631a9cac5958119f06ddbb6a", {duration:100}).then(updatedRecipe => {
+  //     console.log({updatedRecipe});
+  //    mongoose.disconnect();
+  //    }).catch(err => {
+  //     mongoose.disconnect();
+  //     throw err;
+  //   })
+  // })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: String,
+  level: {
+  type: String,
+  enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: [{type: String}]
+  },
+  cuisine: String,
+  dishType: String,
+  enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: Number,
+  creator: String,
+}, {
+  timestamp: true
+})
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
